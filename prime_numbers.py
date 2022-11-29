a0a0430feeac22974ff1723e21c00fca8249b528
aragha <aras2k@yahoo.com>
2022-11-28 20:33:23 -0500

    Updated prime_numbers.py Updated main.py and used the function defined in prime_numbers.py

---
+++ prime_numbers.py
@@ -0,0 +1,20 @@
+import math
+def check_prime(n):
+    root = int(math.sqrt(n))
+    # print(root)
+    for i in range(2, root+1):
+        if n % i == 0:
+            return(False)
+
+    return(True)
+
+# print(check_prime(88))
+# prime_list = []
+# print("Prime          Not Prime")
+# for i in range(2, 1000):
+#     if check_prime(i):
+#         print(i)
+#         prime_list.append(i)
+#     else:
+#         print(f"               {i}")
+# print(prime_list)
